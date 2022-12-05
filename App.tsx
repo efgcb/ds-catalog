0457d521692d976cd361784163d04e7258824303
eduardo.fgoncalves <efgcb@windowslive.com>
2022-12-05 10:29:30 -0300

    App.tsx

--- App.tsx
+++ App.tsx
@@ -1,16 +1,45 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+
+/**
+Quando a Felxdirection é row, o justify content alinha horizontalmente,
+quando é Column alinha verticalmente
+
+
+*/
 
 const App = () => {
   return (
-    <View
-      style={{
-        backgroundColor: "#069",
-      }}
-    >
-      <Text>Hello Word</Text>
+    <View style={styles.container}>
+      <Text style={styles.h1}>Hello Word</Text>
+      <Text style={styles.h2}>Eduardo Fernandes</Text>
+      <Text style={styles.h3}>Aluno do Curso de React Native</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#069",
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center", //horizontal
+    justifyContent: "center", //alinhamento vertical
+  },
+  h1: {
+    fontSize: 24,
+    fontWeight: "700",
+    color: "#fff",
+  },
+  h2: {
+    fontSize: 18,
+    fontWeight: "500",
+    color: "#fff",
+  },
+  h3: {
+    fontSize: 14,
+    color: "#ccc",
+  },
+});
 
 export default App;
