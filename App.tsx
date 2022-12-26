167047da233733941f15dcd3d362132b7cffe939
eduardo.fgoncalves <efgcb@windowslive.com>
2022-12-26 14:20:02 -0300

    React-Navigation

--- App.tsx
+++ App.tsx
@@ -1,45 +1,14 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import Routes from "./src/routes";
 
-/**
-Quando a Felxdirection é row, o justify content alinha horizontalmente,
-quando é Column alinha verticalmente
-
-
-*/
-
-const App = () => {
+const App: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.h1}>Hello Word</Text>
-      <Text style={styles.h2}>Eduardo Fernandes</Text>
-      <Text style={styles.h3}>Aluno do Curso de React Native</Text>
-    </View>
+    <NavigationContainer>
+      <Routes />
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#069",
-    flex: 1,
-    flexDirection: "column",
-    alignItems: "center", //horizontal
-    justifyContent: "center", //alinhamento vertical
-  },
-  h1: {
-    fontSize: 24,
-    fontWeight: "700",
-    color: "#fff",
-  },
-  h2: {
-    fontSize: 18,
-    fontWeight: "500",
-    color: "#fff",
-  },
-  h3: {
-    fontSize: 14,
-    color: "#ccc",
-  },
-});
 
 export default App;
---
+++ src/routes/pages/Catalog.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const Catalog: React.FC = () => {
+  return (
+    <View>
+      <Text>Bem vindo ao APP</Text>
+    </View>
+  );
+};
+
+export default Catalog;
